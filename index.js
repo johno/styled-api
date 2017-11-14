c7a3802b1a3c8919d687e2fccd30683da2d3b217
John Otander <johnotander@gmail.com>
2017-11-14 16:00:19 -0700

    Use ramda's merge fn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const {
   map,
   pipe,
+  merge,
   xprod,
   unnest,
   reduce,
@@ -51,8 +52,7 @@ module.exports.propTypes = (api, theme) =>
 module.exports.xProduct = (api, theme) => {
   const styles = Object.keys(api)
 
-  const combine = (acc, i) => Object.assign({}, acc, i)
-  const reduceObjectArray = arr => reduce(combine, {}, arr)
+  const reduceObjectArray = arr => reduce(merge, {}, arr)
   const xproduct = reduce(pipe(xprod, map(unnest)), [[]])
   const valueByProp = object => (acc, i) => concat(acc, [
     map(v => ({ [i]: v }), object[i])
